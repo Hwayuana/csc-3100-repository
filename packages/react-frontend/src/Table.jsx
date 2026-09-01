d961446c97259a5790a524d11b90e1cdf42de491
Amy <iam_turtles2006@yahoo.com>
2026-08-31 20:45:37 -0700

    IE1 commit

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -17,11 +17,11 @@ function TableBody(props) {
     return (
       <tr key={index}>
   <td>{row.name}</td>
-  <td>{row.job}</td>
-  <td>
-    <button onClick={() => props.removeCharacter(index)}>Delete</button>
-  </td>
-</tr>
+    <td>{row.job}</td>
+      <td>
+        <button onClick={() => props.removeCharacter(index)}>Delete</button>
+     </td>
+    </tr>
     );
    }
   );
